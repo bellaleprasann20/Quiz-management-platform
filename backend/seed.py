f8df90d0181deb97ca0472b0f97fc28faaf09b78
bellaleprasann20 <bellaleprasann20@gmail.com>
2026-08-20 16:45:30 +0530

    seeding database

--- backend/seed.py
+++ backend/seed.py
@@ -1,11 +1,19 @@
 import os
 from sqlalchemy import text
-from app.core.database import SessionLocal
+from app.core.database import SessionLocal, engine, Base
+
+# Forces Python to read your models and know what tables to create
+import app.models 
 
 def run_seed():
+    print("🏗️ Building database tables...")
+    # This creates the empty tables (categories, quizzes, users, etc.)
+    Base.metadata.create_all(bind=engine)
+    
     print("🌱 Reading from database/seed.sql...")
     db = SessionLocal()
-    sql_file_path = os.path.join(os.getcwd(), "database", "seed.sql")
+    
+    sql_file_path = os.path.abspath(os.path.join(os.getcwd(), "..", "database", "seed.sql"))
     
     if not os.path.exists(sql_file_path):
         print(f"⚠️ Error: Could not find {sql_file_path}")
