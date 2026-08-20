f15b27235cf2d5c8c83c67806b70456d4822be54
bellaleprasann20 <bellaleprasann20@gmail.com>
2026-08-21 00:46:02 +0530

    Fixed CORS for all Vercel preview URLs

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,6 +37,7 @@ if frontend_url:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins, 
+    allow_origin_regex=r"https://.*\.vercel\.app",  # Add this magic line!
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
